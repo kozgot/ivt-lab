d67c9d5cb077a01f0458057455caf82dc9340307
kozgot <gogota97@gmail.com>
2019-04-26 09:41:22 +0200

    fixing random

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,7 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import java.util.Random;
-import java.security.SecureRandom;
+
 
 /**
 * Class storing and managing the torpedoes of a ship
@@ -15,7 +15,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
   
-  private Random rand = SecureRandom.getInstanceStrong();
+  private Random rand = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
